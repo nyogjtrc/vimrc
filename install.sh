6affc5b43af1157775d9cba06dee78902f633667
nyo <billy31210@gmail.com>
2013-10-28 22:16:52 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# install pathogen.vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle;
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
-# install .vimrc
-[ -e "~/.vimrc" ] && echo ".vimrc exists." >&2
-[ -e "~/.gvimrc" ] && echo ".gvimrc exists." >&2
-
-
 # create link
+rm ~/.vim
 ln -s ~/vimrc ~/.vim
-ln -s ~/vimrc/.vimrc .vimrc
-ln -s ~/vimrc/.gvimrc .gvimrc
+
+rm ~/.vimrc
+ln -s ~/vimrc/vimrc ~/.vimrc
+
+rm ~/.gvimrc
+ln -s ~/vimrc/gvimrc ~/.gvimrc
